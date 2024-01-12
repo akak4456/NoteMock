b58844a0cdd6f09a2a7a91309d1b46f5d1b764fa
Administrator <akak4456@naver.com>
2024-01-12 12:26:49 +0900

    CNG: build 가 되도록 수정함

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.jo.notemock"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.jo.notemock"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
